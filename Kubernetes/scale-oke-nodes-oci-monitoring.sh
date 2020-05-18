f611ef6e1d1ebbac6a46f34f9264189d6e682800
Oguz Pastirmaci <oguzp@outlook.com>
2020-05-18 16:26:03 -0700

    Create scale-oke-nodes-oci-monitoring.sh

---
+++ Kubernetes/scale-oke-nodes-oci-monitoring.sh
@@ -0,0 +1,2 @@
+# Check the number of API Server Requests metric with a 5 minute interval
+oci monitoring metric-data summarize-metrics-data --namespace oci_oke --compartment-id ocid1.compartment.oc1..aaaaaaaafxezajxlyjxkh23ux75iqttja2qvdded3fc3v5h4kth6zvhnus3q --query-text='(APIServerRequestCount[5m]{ clusterId="ocid1.cluster.oc1.iad.aaaaaaaaae4deyzygiztszjxgu2wemzrmi4windcmvrdan3cgctdqmrumfsg"}.rate() )' | jq -r '.data[]."aggregated-datapoints"[].value'
