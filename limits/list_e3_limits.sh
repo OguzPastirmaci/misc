acb882465789fde7407ce4b88c22069e6376fb8f
Oguz Pastirmaci <oguzp@outlook.com>
2021-02-11 12:41:53 -0800

    Update list_e3_limits.sh

--- limits/list_e3_limits.sh
+++ limits/list_e3_limits.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+TENANCY_ID=""
 OCI_CONFIG_FILE=~/.oci/config
 PROFILE=DEFAULT
+
 REGION=$1
+COMPARTMENT_ID=$2
 
 YELLOW="\033[93m"
 RED="\033[91m"
@@ -17,12 +20,12 @@ fi
 # Check if OCI config file exists
 [ ! -f "$OCI_CONFIG_FILE" ] && echo -e "\nOCI config file does not exist in $OCI_CONFIG_FILE" && exit 1
 
-TENANCY_ID=$(grep -E "^\[|ocid1.tenancy" $OCI_CONFIG_FILE|sed -n -e "/\[$PROFILE\]/,/tenancy/p"|tail -1| awk -F'=' '{ print $2 }' | sed 's/ //g')
 
 list_limits()
 {
 
 local lregion=$1
+local lcompartment=$2
 
 ADS=$(oci --profile $PROFILE iam availability-domain list --region $lregion  | jq '.data[].name'|sed 's#"##g')
 
@@ -32,16 +35,17 @@ for AD in $ADS
 do 
     printf "\n${YELLOW}E3 Compute Limits for AD: $AD\n\n${NORMAL}"
     oci --profile $PROFILE limits resource-availability get --limit-name standard-e3-core-ad-count --service-name compute --region $lregion --availability-domain $AD \
-        --compartment-id $TENANCY_ID --output table
+        --compartment-id $lcompartment --output table
+    
     printf "\n${YELLOW}E3 Memory Limits for AD: $AD\n\n${NORMAL}"
     oci --profile $PROFILE limits resource-availability get --limit-name standard-e3-memory-count --service-name compute --region $lregion --availability-domain $AD \
-        --compartment-id $TENANCY_ID --output table
+        --compartment-id $lcompartment --output table
 done
 }
 
 if [ "$1" == "--all" ]
 then
-  REGIONS_LIST=$(oci --profile $PROFILE iam region-subscription list --query "data [].{Region:\"region-name\"}" |jq -r '.[].Region')
+  REGIONS_LIST=$(oci --profile $PROFILE iam region-subscription list --tenancy-id $TENANCY_ID --query "data [].{Region:\"region-name\"}" |jq -r '.[].Region')
   
   printf "${YELLOW}List of active regions in the tenancy:${NORMAL}\n\n"
   
@@ -49,8 +53,8 @@ then
 
   for region in $REGIONS_LIST
   do
-    list_limits $region 
+    list_limits $region $COMPARTMENT_ID
   done
 else
-list_limits $REGION
+list_limits $REGION $COMPARTMENT_ID
 fi
