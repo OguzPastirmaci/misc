3c50bb789eefe1c5d94d9d2d3c2bf3c7bb25e5c7
Oguz Pastirmaci <oguzp@outlook.com>
2022-08-03 09:24:40 -0700

    Update oci_rhel.sh

--- oci_rhel.sh
+++ oci_rhel.sh
@@ -12,7 +12,7 @@ echo 'add_dracutmodules+="iscsi"' >> /etc/dracut.conf.d/iscsi.conf
 echo "ISCSI Modules Added to Dracut"
 cat /etc/default/grub | grep -v 'GRUB_SERIAL_COMMAND\|GRUB_TERMINAL\|GRUB_CMDLINE_LINUX' > /tmp/grub
 echo 'GRUB_TERMINAL="console"' >> /tmp/grub
-echo 'GRUB_CMDLINE_LINUX="crashkernel=auto ip=dhcp LANG=en_US.UTF-8 console=tty0 console=ttyS0,9600 iommu=on systemd.log_level=debug systemd.log_target=kmsg log_buf_len=100M netroot=iscsi:@169.254.0.2::::iqn.2015-02.oracle.boot:uefi iscsi_initiator=iqn.2015- 02.oracle.boot:instance"' >> /tmp/grub
+echo 'GRUB_CMDLINE_LINUX="crashkernel=auto ip=dhcp LANG=en_US.UTF-8 console=tty0 console=ttyS0,9600 iommu=on systemd.log_level=debug systemd.log_target=kmsg log_buf_len=100M netroot=iscsi:@169.254.0.2::::iqn.2015-02.oracle.boot:uefi iscsi_initiator=iqn.2015-02.oracle.boot:instance"' >> /tmp/grub
 cp /tmp/grub /etc/default/grub
 grub2-mkconfig -o /etc/grub2-efi.cfg
 echo "Grub Config Made"
