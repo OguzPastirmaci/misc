af4bf6457a1b15dc1c71b50adefea8a84f708d46
Oguz Pastirmaci <oguzp@outlook.com>
2024-09-20 18:47:32 -0700

    Update oke-cloud-init.sh

--- oke-cloud-init.sh
+++ oke-cloud-init.sh
@@ -1,4 +1,3 @@
-  GNU nano 6.2                                                                       /var/run/oke-init.sh *
 #!/bin/bash
 
 add-apt-repository -y 'deb [trusted=yes] https://odx-oke.objectstorage.us-sanjose-1.oci.customer-oci.com/n/odx-oke/b/okn-repositories/o/prod/ubuntu-jammy/kubernetes-1.29 stable main'
