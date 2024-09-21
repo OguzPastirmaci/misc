b4cd1c8be9323d78d848958e0e2a37472a553b9f
Oguz Pastirmaci <oguzp@outlook.com>
2024-09-20 18:36:18 -0700

    Update oke-cloud-init.sh

--- oke-cloud-init.sh
+++ oke-cloud-init.sh
@@ -2,7 +2,7 @@
 
 add-apt-repository -y 'deb [trusted=yes] https://odx-oke.objectstorage.us-sanjose-1.oci.customer-oci.com/n/odx-oke/b/okn-repositories/o/prod/ubuntu-jammy/kubernetes-1.29 stable main'
 
-apt update && apt install -y oci-oke-node-all*
+apt-get -o DPkg::Lock::Timeout=60 update && apt-get -o DPkg::Lock::Timeout=60 install -y oci-oke-node-all*
 
 cat <<EOF > /etc/containers/storage.conf
 [storage]
