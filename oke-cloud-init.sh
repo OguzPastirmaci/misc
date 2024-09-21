b16039b30487ad57d7ff5b5d11316c8a6bed75bd
Oguz Pastirmaci <oguzp@outlook.com>
2024-09-21 10:20:33 -0700

    Update oke-cloud-init.sh

--- oke-cloud-init.sh
+++ oke-cloud-init.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Wait for apt lock and install the package
-while fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
-   sleep 1
-done
+#while fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
+#   sleep 1
+#done
 
 # Add OKE Ubuntu package repo
 add-apt-repository -y 'deb [trusted=yes] https://odx-oke.objectstorage.us-sanjose-1.oci.customer-oci.com/n/odx-oke/b/okn-repositories/o/prod/ubuntu-jammy/kubernetes-1.29 stable main'
 
-apt-get update && apt-get install -y oci-oke-node-all*
+apt-get -y -o DPkg::Lock::Timeout=-1 update
+
+apt-get -y -o DPkg::Lock::Timeout=-1 install oci-oke-node-all*
 
 # Use the first Nvme drive (/dev/nvme0n1) for CRI-O if it exists
 #if [ -e '/dev/nvme0n1' ]; then
