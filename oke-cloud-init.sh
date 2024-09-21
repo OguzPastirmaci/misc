376cd3bca657a33fe9e6777dbf608d934c5a306a
Oguz Pastirmaci <oguzp@outlook.com>
2024-09-20 22:23:42 -0700

    Update oke-cloud-init.sh

--- oke-cloud-init.sh
+++ oke-cloud-init.sh
@@ -11,17 +11,17 @@ add-apt-repository -y 'deb [trusted=yes] https://odx-oke.objectstorage.us-sanjos
 apt-get update && apt-get install -y oci-oke-node-all*
 
 # Use the first Nvme drive (/dev/nvme0n1) for CRI-O if it exists
-if [ -e '/dev/nvme0n1' ]; then
-    echo "/dev/nvme0n1 found, configuring it for CRI-O"
-    mkdir -p /var/lib/oke-crio
-    parted -a opt --script /dev/nvme0n1 mklabel gpt mkpart primary 0% 100%
-    mkfs.ext4 /dev/nvme0n1p1
-    mount /dev/nvme0n1p1 /var/lib/oke-crio
-    echo "/dev/nvme0n1p1 /var/lib/oke-crio ext4 rw,noatime,nofail 0 2" | tee -a /etc/fstab
-else
-    echo "/dev/nvme0n1 not found"
-    mkdir -p /var/lib/oke-crio
-fi
+#if [ -e '/dev/nvme0n1' ]; then
+#    echo "/dev/nvme0n1 found, configuring it for CRI-O"
+#    mkdir -p /var/lib/oke-crio
+#    parted -a opt --script /dev/nvme0n1 mklabel gpt mkpart primary 0% 100%
+#    mkfs.ext4 /dev/nvme0n1p1
+#    mount /dev/nvme0n1p1 /var/lib/oke-crio
+#    echo "/dev/nvme0n1p1 /var/lib/oke-crio ext4 rw,noatime,nofail 0 2" | tee -a /etc/fstab
+#else
+#    echo "/dev/nvme0n1 not found"
+#    mkdir -p /var/lib/oke-crio
+#fi
 
 # Edit storage.conf to use the first Nvme drive (if it exists) for container images
 cat <<EOF > /etc/containers/storage.conf
