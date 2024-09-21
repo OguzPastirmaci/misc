5a32b69b8a5d984f0eb1c5ef5935ede7dc37849d
Oguz Pastirmaci <oguzp@outlook.com>
2024-09-20 19:58:57 -0700

    Update oke-cloud-init.sh

--- oke-cloud-init.sh
+++ oke-cloud-init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Add OKE Ubuntu package repo
-add-apt-repository -y 'deb [trusted=yes] https://odx-oke.objectstorage.us-sanjose-1.oci.customer-oci.com/n/odx-oke/b/okn-repositories/o/prod/ubuntu-jammy/kubernetes-1.29 stable main'
-
 # Wait for apt lock and install the package
 while fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
    sleep 1
 done
+
+# Add OKE Ubuntu package repo
+add-apt-repository -y 'deb [trusted=yes] https://odx-oke.objectstorage.us-sanjose-1.oci.customer-oci.com/n/odx-oke/b/okn-repositories/o/prod/ubuntu-jammy/kubernetes-1.29 stable main'
 
 apt-get update && apt-get install -y oci-oke-node-all*
 
