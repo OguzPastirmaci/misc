5c8e3b61737e9f62fb3a5cd8e2523b83b4b35afa
Oguz Pastirmaci <oguzp@outlook.com>
2024-09-20 18:02:12 -0700

    Create oke-cloud-init.sh

---
+++ oke-cloud-init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+add-apt-repository -y 'deb [trusted=yes] https://odx-oke.objectstorage.us-sanjose-1.oci.customer-oci.com/n/odx-oke/b/okn-repositories/o/prod/ubuntu-jammy/kubernetes-1.29 stable main'
+
+apt install -y oci-oke-node-all*
+
+cat << EOF > /etc/containers/storage.conf
+[storage]
+# Default storage driver
+driver = "overlay"
+# Temporary storage location
+runroot = "/var/run/containers/storage"
+# Primary read/write location of container storage 
+graphroot = "/var/lib/oke-crio"
+EOF
+
+oke bootstrap --manage-gpu-services
