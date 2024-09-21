262df53d194d16d68349265268ed82041c4d4ef5
Oguz Pastirmaci <oguzp@outlook.com>
2024-09-21 12:56:43 -0700

    Update oke-cloud-init.sh

--- oke-cloud-init.sh
+++ oke-cloud-init.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# Wait for apt lock and install the package
-#while fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
-#   sleep 1
-#done
 distrib_codename=$(lsb_release -c -s)
 kubernetes_version=$1
-oke_package_version="${kubernetes_version:1:4}"
+oke_package_version="${kubernetes_version:1}"
+echo $oke_package_version
+oke_package_repo_version="${oke_package_version:0:4}"
+echo $oke_package_repo_version
 oke_package_name="oci-oke-node-all-$oke_package_version"
-oke_package_repo="https://odx-oke.objectstorage.us-sanjose-1.oci.customer-oci.com/n/odx-oke/b/okn-repositories/o/prod/ubuntu-$distrib_codename/kubernetes-$oke_package_version"
+oke_package_repo="https://odx-oke.objectstorage.us-sanjose-1.oci.customer-oci.com/n/odx-oke/b/okn-repositories/o/prod/ubuntu-$distrib_codename/kubernetes-$oke_package_repo_version"
 
 # Add OKE Ubuntu package repo
 add-apt-repository -y "deb [trusted=yes] $oke_package_repo stable main"
@@ -16,19 +15,6 @@ add-apt-repository -y "deb [trusted=yes] $oke_package_repo stable main"
 apt-get -y -o DPkg::Lock::Timeout=-1 update
 
 apt-get -y -o DPkg::Lock::Timeout=-1 install $oke_package_name
-
-# Use the first Nvme drive (/dev/nvme0n1) for CRI-O if it exists
-#if [ -e '/dev/nvme0n1' ]; then
-#    echo "/dev/nvme0n1 found, configuring it for CRI-O"
-#    mkdir -p /var/lib/oke-crio
-#    parted -a opt --script /dev/nvme0n1 mklabel gpt mkpart primary 0% 100%
-#    mkfs.ext4 /dev/nvme0n1p1
-#    mount /dev/nvme0n1p1 /var/lib/oke-crio
-#    echo "/dev/nvme0n1p1 /var/lib/oke-crio ext4 rw,noatime,nofail 0 2" | tee -a /etc/fstab
-#else
-#    echo "/dev/nvme0n1 not found"
-#    mkdir -p /var/lib/oke-crio
-#fi
 
 # Edit storage.conf to use the first Nvme drive (if it exists) for container images
 cat <<EOF > /etc/containers/storage.conf
