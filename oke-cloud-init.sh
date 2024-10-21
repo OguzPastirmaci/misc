76ec9257d04df556ae30e2410691a64a8000c94d
Oguz Pastirmaci <oguzp@outlook.com>
2024-10-21 13:46:42 -0700

    Update oke-cloud-init.sh

--- oke-cloud-init.sh
+++ oke-cloud-init.sh
@@ -7,6 +7,8 @@ oke_package_repo_version="${oke_package_version:0:4}"
 oke_package_name="oci-oke-node-all-$oke_package_version"
 oke_package_repo="https://odx-oke.objectstorage.us-sanjose-1.oci.customer-oci.com/n/odx-oke/b/okn-repositories/o/prod/ubuntu-$distrib_codename/kubernetes-$oke_package_repo_version"
 
+touch /var/log/inittest1
+
 # Add OKE Ubuntu package repo
 add-apt-repository -y "deb [trusted=yes] $oke_package_repo stable main"
 
