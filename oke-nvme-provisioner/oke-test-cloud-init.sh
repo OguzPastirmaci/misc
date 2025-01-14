5b98118600ed57d107db9971dd091be612da41b3
Oguz Pastirmaci <oguzp@outlook.com>
2025-01-14 14:18:59 -0800

    Update oke-test-cloud-init.sh

--- oke-nvme-provisioner/oke-test-cloud-init.sh
+++ oke-nvme-provisioner/oke-test-cloud-init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Add OKE repo & install the package
-add-apt-repository -y 'deb [trusted=yes] https://odx-oke.objectstorage.us-sanjose-1.oci.customer-oci.com/n/odx-oke/b/okn-repositories/o/prod/ubuntu-jammy/kubernetes-1.29 stable main'
+add-apt-repository -y 'deb [trusted=yes] https://odx-oke.objectstorage.us-sanjose-1.oci.customer-oci.com/n/odx-oke/b/okn-repositories/o/prod/ubuntu-jammy/kubernetes-1.28 stable main'
 
 while fuser /var/{lib/{dpkg/{lock,lock-frontend},apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
    echo "Waiting for dpkg/apt lock"
