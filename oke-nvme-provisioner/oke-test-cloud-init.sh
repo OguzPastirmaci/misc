4f9ceaccf95fc20f2f5b4eb38bea3a9a9035e992
Oguz Pastirmaci <oguzp@outlook.com>
2025-02-08 12:14:25 -0800

    Update oke-test-cloud-init.sh

--- oke-nvme-provisioner/oke-test-cloud-init.sh
+++ oke-nvme-provisioner/oke-test-cloud-init.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
+set -x
 
-# Add OKE repo & install the package
-add-apt-repository -y 'deb [trusted=yes] https://odx-oke.objectstorage.us-sanjose-1.oci.customer-oci.com/n/odx-oke/b/okn-repositories/o/prod/ubuntu-jammy/kubernetes-1.29 stable main'
+distrib_codename=$(lsb_release -c -s)
+kubernetes_version=$1
+oke_package_version="${kubernetes_version:1}"
+oke_package_repo_version="${oke_package_version:0:4}"
+oke_package_name="oci-oke-node-all-$oke_package_version"
+oke_package_repo="https://odx-oke.objectstorage.us-sanjose-1.oci.customer-oci.com/n/odx-oke/b/okn-repositories/o/prod/ubuntu-$distrib_codename/kubernetes-$oke_package_repo_version"
 
+# Add OKE Ubuntu package repo
+add-apt-repository -y "deb [trusted=yes] $oke_package_repo stable main"
+
+# Wait for apt lock and install the package
 while fuser /var/{lib/{dpkg/{lock,lock-frontend},apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
    echo "Waiting for dpkg/apt lock"
    sleep 1
 done
 
-apt update
-apt install -y oci-oke-node-all*
+apt-get -y update
 
-oke bootstrap
+apt-get -y install $oke_package_name
+
+# OKE bootstrap with hostname instead of IP for node names
+oke bootstrap --kubelet-extra-args "--hostname-override $(hostname)"
