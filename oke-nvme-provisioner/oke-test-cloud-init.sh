ce64e6feb406b88ae8a7e5303d23469cbffc09e7
Oguz Pastirmaci <oguzp@outlook.com>
2025-03-27 13:40:09 -0700

    Update oke-test-cloud-init.sh

--- oke-nvme-provisioner/oke-test-cloud-init.sh
+++ oke-nvme-provisioner/oke-test-cloud-init.sh
@@ -3,11 +3,10 @@ set -x
 
 distrib_codename=$(lsb_release -c -s)
 kubernetes_version=$1
-override_hostnames=$2
 oke_package_version="${kubernetes_version:1}"
 oke_package_repo_version="${oke_package_version:0:4}"
 oke_package_name="oci-oke-node-all-$oke_package_version"
-oke_package_repo="https://odx-oke.objectstorage.us-sanjose-1.oci.customer-oci.com/n/odx-oke/b/okn-repositories/o/prod/ubuntu-$distrib_codename/kubernetes-$oke_package_repo_version"
+oke_package_repo="https://objectstorage.us-sanjose-1.oraclecloud.com/p/45eOeErEDZqPGiymXZwpeebCNb5lnwzkcQIhtVf6iOF44eet_efdePaF7T8agNYq/n/odx-oke/b/okn-repositories-private/o/prod//ubuntu-$distrib_codename/kubernetes-$oke_package_repo_version"
 
 # Add OKE Ubuntu package repo
 add-apt-repository -y "deb [trusted=yes] $oke_package_repo stable main"
@@ -23,10 +22,4 @@ apt-get -y update
 apt-get -y install $oke_package_name
 
 # OKE bootstrap
-if [ "$override_hostnames" == true ]; then
-    oke bootstrap --kubelet-extra-args "--hostname-override $(hostname)"
-else
-    oke bootstrap
-fi
-
-
+oke bootstrap
