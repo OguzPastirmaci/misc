bc75c70c8537eae559e27e12d2342803ecfc6b95
Oguz Pastirmaci <oguzp@outlook.com>
2025-04-03 11:16:02 -0700

    Update oke-test-cloud-init.sh

--- oke-nvme-provisioner/oke-test-cloud-init.sh
+++ oke-nvme-provisioner/oke-test-cloud-init.sh
@@ -6,7 +6,7 @@ kubernetes_version=$1
 oke_package_version="${kubernetes_version:1}"
 oke_package_repo_version="${oke_package_version:0:4}"
 oke_package_name="oci-oke-node-all-$oke_package_version"
-oke_package_repo="https://objectstorage.us-sanjose-1.oraclecloud.com/p/45eOeErEDZqPGiymXZwpeebCNb5lnwzkcQIhtVf6iOF44eet_efdePaF7T8agNYq/n/odx-oke/b/okn-repositories-private/o/prod//ubuntu-$distrib_codename/kubernetes-$oke_package_repo_version"
+oke_package_repo="https://odx-oke.objectstorage.us-sanjose-1.oci.customer-oci.com/n/odx-oke/b/okn-repositories/o/prod/ubuntu-$distrib_codename/kubernetes-$oke_package_repo_version"
 
 # Add OKE Ubuntu package repo
 add-apt-repository -y "deb [trusted=yes] $oke_package_repo stable main"
