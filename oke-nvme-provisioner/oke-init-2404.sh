d1cea05b445d9d7d7d19bd10c5fd525ffad199f0
Oguz Pastirmaci <oguzp@outlook.com>
2025-05-05 23:21:59 -0700

    Update oke-init-2404.sh

--- oke-nvme-provisioner/oke-init-2404.sh
+++ oke-nvme-provisioner/oke-init-2404.sh
@@ -3,7 +3,6 @@ set -x
 
 source /etc/os-release
 
-#distrib_codename=$(lsb_release -c -s)
 kubernetes_version=$1
 oke_package_version="${kubernetes_version:1}"
 oke_package_repo_version="${oke_package_version:0:4}"
@@ -11,7 +10,14 @@ oke_package_name="oci-oke-node-all-$oke_package_version"
 oke_package_repo="https://odx-oke.objectstorage.us-sanjose-1.oci.customer-oci.com/n/odx-oke/b/okn-repositories/o/prod/ubuntu-$VERSION_CODENAME/kubernetes-$oke_package_repo_version"
 
 # Add OKE Ubuntu package repo
-add-apt-repository -y "deb [trusted=yes] $oke_package_repo stable main"
+tee -a /etc/apt/sources.list.d/oke-node-client.sources <<EOF
+Enabled: yes
+Types: deb
+URIs: https://odx-oke.objectstorage.us-sanjose-1.oci.customer-oci.com/n/odx-oke/b/okn-repositories/o/prod/ubuntu-$VERSION_CODENAME/kubernetes-$oke_package_repo_version
+Suites: stable
+Components: main
+Trusted: yes
+EOF
 
 # Wait for apt lock and install the package
 while fuser /var/{lib/{dpkg/{lock,lock-frontend},apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
