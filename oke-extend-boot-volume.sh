b8f2384c0e8de7527f99d656a452e12f6a737d5a
Oguz Pastirmaci <oguzp@outlook.com>
2025-06-18 11:50:56 -0700

    Create oke-extend-boot-volume.sh

---
+++ oke-extend-boot-volume.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo dd iflag=direct if=/dev/oracleoci/oraclevda of=/dev/null count=1
+echo "1" | sudo tee /sys/class/block/`readlink /dev/oracleoci/oraclevda | cut -d'/' -f 2`/device/rescan
+
+sudo growpart /dev/sda 1
+
+sudo resize2fs /dev/sda1
